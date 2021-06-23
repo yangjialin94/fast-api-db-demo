51e7a8ff886db9161026e182d904763e4bfe5705
Jialin Yang <yangjialin94@gmail.com>
2021-06-23 11:40:45 -0500

    added user

--- blog/main.py
+++ blog/main.py
@@ -1,7 +1,9 @@
+from typing import List
 from fastapi import FastAPI, Depends, status, Response, HTTPException
 from . import schemas, models
 from .database import engine, SessionLocal
 from sqlalchemy.orm import Session
+from .hashing import Hash
 
 app = FastAPI()
 
@@ -54,13 +56,13 @@ def update(id, request: schemas.Blog, db: Session = Depends(get_db)):
     return "Updated"
 
 
-@app.get("/blog")
+@app.get("/blog", response_model=List[schemas.ShowBlog])
 def all(db: Session = Depends(get_db)):
     blogs = db.query(models.Blog).all()
     return blogs
 
 
-@app.get("/blog/{id}", status_code=status.HTTP_200_OK)
+@app.get("/blog/{id}", status_code=status.HTTP_200_OK, response_model=schemas.ShowBlog)
 def show(id, db: Session = Depends(get_db)):
     blog = db.query(models.Blog).filter(models.Blog.id == id).first()
     if not blog:
@@ -69,3 +71,27 @@ def show(id, db: Session = Depends(get_db)):
             detail=f"Blog with the id {id} is not available",
         )
     return blog
+
+
+@app.post("/user", response_model=schemas.ShowUser)
+def create_user(request: schemas.User, db: Session = Depends(get_db)):
+    new_user = models.User(
+        name=request.name,
+        email=request.email,
+        password=Hash.bcrypt(request.password),
+    )
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+    return new_user
+
+
+@app.get("/user/{id}", status_code=status.HTTP_200_OK, response_model=schemas.ShowUser)
+def show(id, db: Session = Depends(get_db)):
+    user = db.query(models.User).filter(models.User.id == id).first()
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"User with the id {id} is not available",
+        )
+    return user
