86948d56e7e7625f51e5f048e11f4a469d5e7bd7
Jialin Yang <yangjialin94@gmail.com>
2021-06-23 08:50:21 -0500

    first commit

---
+++ blog/main.py
@@ -0,0 +1,40 @@
+from fastapi import FastAPI, Depends, status
+from . import schemas, models
+from .database import engine, SessionLocal
+from sqlalchemy.orm import Session
+
+app = FastAPI()
+
+# create all the models into the db
+# migrating all the tables
+models.Base.metadata.create_all(engine)
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+# store the blog information into the db
+@app.post("/blog", status_code=status.HTTP_201_CREATED)
+def create(request: schemas.Blog, db: Session = Depends(get_db)):
+    new_blog = models.Blog(title=request.title, body=request.body)
+    db.add(new_blog)
+    db.commit()
+    db.refresh(new_blog)
+    return new_blog
+
+
+@app.get("/blog")
+def all(db: Session = Depends(get_db)):
+    blogs = db.query(models.Blog).all()
+    return blogs
+
+
+@app.get("/blog/{id}")
+def show(id, db: Session = Depends(get_db)):
+    blog = db.query(models.Blog).filter(models.Blog.id == id).first()
+    return blog
---
+++ main.py
@@ -0,0 +1,53 @@
+from fastapi import FastAPI
+from typing import Optional
+from pydantic import BaseModel
+import uvicorn
+
+
+app = FastAPI()
+
+
+# path operation decorator: @app
+# operation: get
+# path: ("/blog")
+@app.get("/blog")
+# path operation function
+def index(limit=10, published: bool = True, sort: Optional[str] = None):
+    # only get 10 published blogs
+    if published:
+        return {"data": f"{limit} published blogs from the db"}
+    else:
+        return {"data": f"{limit} blogs from the db"}
+
+
+@app.get("/blog/unpublished")
+def unpublished():
+    return {"data": "all unpublished blogs"}
+
+
+@app.get("/blog/{id}")
+def show(id: int):
+    # fetch blog with id = id
+    return {"data": id}
+
+
+@app.get("/blog/{id}/comments")
+def comments(id, limit=10):
+    # fetch blog with id = id
+    return {"data": {"1", "2"}}
+
+
+class Blog(BaseModel):
+    title: str
+    body: str
+    published: Optional[bool]
+
+
+@app.post("/blog")
+# request body: blog
+def create_blog(blog: Blog):
+    return {"data": f"Blog is creawted with title as {blog.title}"}
+
+
+# if __name__ == "__main__":
+#     uvicorn.run(app, host="127.0.0.1", port=9000)
