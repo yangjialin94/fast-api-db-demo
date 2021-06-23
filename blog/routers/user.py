794a4ed35d5bde8ef5ca960efc593f6aa0a5e0b5
Jialin Yang <yangjialin94@gmail.com>
2021-06-23 13:38:18 -0500

    added router

---
+++ blog/routers/user.py
@@ -0,0 +1,38 @@
+from typing import List
+from fastapi import APIRouter, Depends, status, HTTPException
+from .. import schemas, database, models
+from sqlalchemy.orm import Session
+from ..hashing import Hash
+
+
+router = APIRouter()
+get_db = database.get_db
+
+
+@router.post("/user", response_model=schemas.ShowUser, tags=["users"])
+def create_user(request: schemas.User, db: Session = Depends(get_db)):
+    new_user = models.User(
+        name=request.name,
+        email=request.email,
+        password=Hash.bcrypt(request.password),
+    )
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+    return new_user
+
+
+@router.get(
+    "/user/{id}",
+    status_code=status.HTTP_200_OK,
+    response_model=schemas.ShowUser,
+    tags=["users"],
+)
+def show(id, db: Session = Depends(get_db)):
+    user = db.query(models.User).filter(models.User.id == id).first()
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"User with the id {id} is not available",
+        )
+    return user
